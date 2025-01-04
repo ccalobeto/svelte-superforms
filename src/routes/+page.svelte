0577c794c4212a38c4b5c9deb2f5e5cc4372857c
ccalobeto <calobeto.leon@gmail.com>
2025-01-04 05:50:34 -0500

    building a form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div class="form-wrapper">
+	<form method="POST">
+		<label for="name">First Name</label>
+		<input type="text" name="name" />
+
+		<label for="name">Last Name</label>
+		<input type="text" name="name" />
+
+		<label for="email">E-mail</label>
+		<input type="email" name="email" />
+
+		<label for="email">Employee Number</label>
+		<input type="number" name="employeeNumber" />
+
+		<div><button>Submit</button></div>
+	</form>
+</div>
+
+<style>
+	.form-wrapper {
+		display: flex;
+		justify-content: center;
+	}
+	button {
+		background-color: #008001;
+		border-radius: 5px;
+		border: none;
+		padding: 10px;
+	}
+	button:hover {
+		background-color: #aad922;
+	}
+</style>
