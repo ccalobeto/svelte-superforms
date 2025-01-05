c2d2ec8e3d2afab377cd81bc6b20943fdeb63c78
ccalobeto <calobeto.leon@gmail.com>
2025-01-05 16:18:54 -0500

    add actions

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,13 +1,26 @@
-import type { PageServerLoad } from "./$types";
+import type { PageServerLoad, Actions } from "./$types";
 import { superValidate } from "sveltekit-superforms/server"
 import { zod } from "sveltekit-superforms/adapters";
 import { userSchema } from "./schema";
-
+import { fail } from "@sveltejs/kit";
 
 export const load: PageServerLoad = (async () => {
   // Server API:
   const form = await superValidate(zod(userSchema));
-
   // Always return { form } in load and form actions.
   return { form };
 });
+
+export const actions: Actions = {
+  default: async (event) => {
+    const form = await superValidate(event, zod(userSchema));
+    if (!form.valid) {
+      return fail(400, {
+        form,
+      });
+    }
+    return {
+      form,
+    };
+  },
+};
