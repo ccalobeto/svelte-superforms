7b63020d8fe5bd309e928fb0c58aacc34857118f
ccalobeto <calobeto.leon@gmail.com>
2025-01-08 18:05:23 -0500

    insert another route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,10 @@
 
 <h1>Welcome to svelte kit</h1>
 <p>Visit <a href="./contact">Contact Form</a></p>
+<p>Visit <a href="./products">Products</a></p>
+
+<style>
+	a {
+		color: rgb(25, 0, 255);
+	}
+</style>
