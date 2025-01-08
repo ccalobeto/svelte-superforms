dc7793acf05bb45f310679e313b868cea52f0325
ccalobeto <calobeto.leon@gmail.com>
2025-01-08 11:31:54 -0500

    fix contact route in deployment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<!-- <script>
+	// import { base } from '$app/paths';
+</script> -->
+
 <h1>Welcome to svelte kit</h1>
-<p>Visit <a href="/contact">Contact Form</a></p>
+<p>Visit <a href="./contact">Contact Form</a></p>
