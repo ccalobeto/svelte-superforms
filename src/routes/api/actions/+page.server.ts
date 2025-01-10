fdc74cb233e3d2483cd70e16efda0509060740cc
ccalobeto <calobeto.leon@gmail.com>
2025-01-09 19:00:15 -0500

    WIP fix form actions in deployment

---
+++ src/routes/api/actions/+page.server.ts
@@ -0,0 +1,31 @@
+export const prerender = false;
+
+import type { Actions } from "./$types";
+import { superValidate } from "sveltekit-superforms/server"
+import { zod } from "sveltekit-superforms/adapters";
+import { emailSchema } from "$lib/utils/schema";
+import { fail } from "@sveltejs/kit";
+import emailHelper from "$lib/utils/emailHelper";
+
+export const actions: Actions = {
+  sendEmail: async (event) => {
+    const form = await superValidate(event, zod(emailSchema));
+    if (!form.valid) {
+      return fail(400, {
+        form,
+      });
+    }
+    console.log('**this is +page.server.ts**');
+    const { from, subject, message } = form.data;
+    try {
+      const emailResponse = await emailHelper(from, subject, message);
+      console.log('message sent from ', emailResponse.envelope.from);
+    } catch (error) {
+      console.log(error);
+    }
+
+    return {
+      form,
+    };
+  },
+};
