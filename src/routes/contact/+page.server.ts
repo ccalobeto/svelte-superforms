196780418badef5362b382bb1d9a58f184f84a98
ccalobeto <calobeto.leon@gmail.com>
2025-01-14 04:05:21 -0500

    WIP fix actions in deployment

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,12 +1,10 @@
-export const prerender = false;
-export const ssr = false;
+// export const prerender = true;
+// export const ssr = false;
 
-import type { PageServerLoad, Actions } from "./$types";
+import type { PageServerLoad } from "./$types";
 import { superValidate } from "sveltekit-superforms/server"
 import { zod } from "sveltekit-superforms/adapters";
 import { emailSchema } from "../../lib/utils/schema";
-import { fail } from "@sveltejs/kit";
-import emailHelper from "$lib/utils/emailHelper";
 
 export const load: PageServerLoad = (async () => {
   // Server API:
@@ -15,25 +13,3 @@ export const load: PageServerLoad = (async () => {
   return { form };
 });
 
-export const actions: Actions = {
-  sendEmail: async (event) => {
-    const form = await superValidate(event, zod(emailSchema));
-    if (!form.valid) {
-      return fail(400, {
-        form,
-      });
-    }
-    console.log('**this is +page.server.ts**');
-    const { from, subject, message } = form.data;
-    try {
-      const emailResponse = await emailHelper(from, subject, message);
-      console.log('message sent from ', emailResponse.envelope.from);
-    } catch (error) {
-      console.log(error);
-    }
-
-    return {
-      form,
-    };
-  },
-};
